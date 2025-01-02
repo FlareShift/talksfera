9258d1c85320b6c7653a63de5d797929591f50f9
TsAndriy <a.andriy.t@gmail.com>
2025-01-02 14:04:49 +0200

    Add Swagger

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.views import APIView
 
+from .models import User, Sex
+from .serializers import UserSerializer
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
